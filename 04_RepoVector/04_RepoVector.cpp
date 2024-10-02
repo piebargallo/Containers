333ab40fa146fe020137d09f1cf0b5492db7bb88
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-10-02 18:04:03 +0200

    Add files via upload

---
+++ 04_RepoVector/04_RepoVector.cpp
@@ -0,0 +1,9 @@
+// Constructors and destructors, constructor with values
+#include <vector>
+using std::vector;
+
+int main()
+{
+	// creates a vector of 10 ints with value
+	vector<int> intVector(10, 100);
+}
