3e1610a51d1780debb417c4fe66eb6b5b420fbfb
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-10-02 18:03:24 +0200

    Add files via upload

---
+++ 03_RepoVector/03_RepoVector.cpp
@@ -0,0 +1,10 @@
+// Constructors and destructors, default constructor
+#include <vector>
+using std::vector;
+
+int main()
+{
+	// creates a vector of ints with zero elements
+	vector<int> intVector; 
+}
+
